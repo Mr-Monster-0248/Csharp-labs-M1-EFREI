f1410f240c2841f3cebe90588aa3dfcfca84893c
Ruben Nabet <rubenabet@gmail.com>
2020-11-21 16:23:15 +0100

    Add TP4 basic project

---
+++ TP4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace TP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

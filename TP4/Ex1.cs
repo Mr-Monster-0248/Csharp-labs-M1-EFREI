43c24869e0f98a95a8010ae296f4b61e2d06b58f
Ruben Nabet <rubenabet@gmail.com>
2020-11-21 17:16:35 +0100

    Finish Ex1

---
+++ TP4/Ex1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+
+namespace TP4
+{
+    class Ex1
+    {
+        private static int i = 5;
+        public static int I => i;
+
+
+
+        private static object access_grant = new object();
+
+
+
+        public static void ResetIToFive()
+        {
+            i = 5;
+        }
+
+
+
+        public static void Increment()
+        {
+            i++;
+        }
+
+        public static void IncrementSwap()
+        {
+            int reg = i;
+            Thread.Sleep(2000);
+            reg++;
+            i = reg;
+        }
+
+        public static void IncrementSemaphores()
+        {
+            lock(access_grant)
+            {
+                int reg = i;
+                Thread.Sleep(2000);
+                reg++;
+                i = reg;
+            }
+        }
+
+
+
+        public static void Decrement()
+        {
+            i--;
+        }
+
+        public static void DecrementSwap()
+        {
+            int reg = i;
+            Thread.Sleep(2000);
+            reg--;
+            i = reg;
+        }
+
+        public static void DecrementSemaphores()
+        {
+            lock (access_grant)
+            {
+                int reg = i;
+                Thread.Sleep(2000);
+                reg--;
+                i = reg;
+            }
+        }
+    }
+}
--- TP4/Program.cs
+++ TP4/Program.cs
@@ -5,9 +5,69 @@ namespace TP4
 {
     class Program
     {
+        static void Simple()
+        {
+            Console.WriteLine("1.");
+
+            Ex1.ResetIToFive();
+
+            Thread T1_1 = new Thread(new ThreadStart(Ex1.Increment));
+            Thread T2_1 = new Thread(new ThreadStart(Ex1.Decrement));
+            T1_1.Start();
+            T2_1.Start();
+            T1_1.Join();
+            T2_1.Join();
+
+            Console.WriteLine($"i = {Ex1.I}\n");
+        }
+
+
+
+        static void Swap()
+        {
+            Console.WriteLine("2.");
+
+            Ex1.ResetIToFive();
+
+            Thread T1_2 = new Thread(new ThreadStart(Ex1.IncrementSwap));
+            Thread T2_2 = new Thread(new ThreadStart(Ex1.DecrementSwap));
+            T1_2.Start();
+            T2_2.Start();
+            T1_2.Join();
+            T2_2.Join();
+
+            Console.WriteLine($"i = {Ex1.I}\n");
+        }
+
+
+
+        static void Semaphores()
+        {
+            Console.WriteLine("3.");
+
+            Ex1.ResetIToFive();
+
+            Thread T1_3 = new Thread(new ThreadStart(Ex1.IncrementSemaphores));
+            Thread T2_3 = new Thread(new ThreadStart(Ex1.DecrementSemaphores));
+            T1_3.Start();
+            T2_3.Start();
+            T1_3.Join();
+            T2_3.Join();
+
+            Console.WriteLine($"i = {Ex1.I}\n");
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("####  EXERCISE 1  ####");
+
+            Simple();
+
+            Swap();
+
+            Semaphores();
         }
     }
 }
